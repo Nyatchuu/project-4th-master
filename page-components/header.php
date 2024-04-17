c830e5f26063bce3d23d0b3c3ea81b770bbd8b6e
Roman Chitrakar <87511718+Nyatchuu@users.noreply.github.com>
2024-04-17 14:02:37 +0545

    y

--- page-components/header.php
+++ page-components/header.php
@@ -1,7 +1,7 @@
 
 <header>
 <nav>
-            <h2><a href="/project/pages/index.php">Traveller</a></h2>
+            <h2><a href="/project-4th-master/pages/index.php">Traveller</a></h2>
             <ul>
                 <li><a href="/project-4th-master/pages/services.php">Services</a></li>
                 <li><a href="/project-4th-master/pages/about-us.php">About Us</a></li>
